afd198db58ac6d740ea79ddc54a7f9dc78aeac18
Alexander Shirapov <cccp2006_06@mail.ru>
2022-01-12 16:52:19 +0800

    Completed Exercises 7-14

---
+++ lpthw/ex13.py
@@ -0,0 +1,10 @@
+from sys import argv
+# read the WYSS section how run this
+
+script, first, second, third, four = argv
+
+print("The script is called:", script)
+print("You first varable is:", first)
+print("You second variable is:", second)
+print("You third varibale is:", third)
+print("You third varibale is:", four)
